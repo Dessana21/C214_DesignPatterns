69d582496402a6ceb696325d6dfa3afe787ca431
DessanaDev <90843911+DessanaDev@users.noreply.github.com>
2022-12-02 10:35:57 -0300

    first commit

---
+++ DesignPattern_ordenacao/src/main/java/Principal/OrdenarQuickSort.java
@@ -0,0 +1,12 @@
+package Principal;
+
+
+import Algoritmos.QuickSort;
+
+public class OrdenarQuickSort extends Ordenadores {
+
+    // Construtor
+    public OrdenarQuickSort(){
+        algoritmos=new QuickSort();
+    }
+}
